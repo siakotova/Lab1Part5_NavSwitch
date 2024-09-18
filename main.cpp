b6ab4530387a44f402c281bdfa6e7dcdb33216fc
siakotova <87061006+siakotova@users.noreply.github.com>
2024-09-17 21:45:11 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "mbed.h"
+#include "USBMouse.h"
+//USB mouse demo using a 5-way Navigation Switch (Digital Joystick)
+//Needs USB connector breakout with D+, D-, and Gnd to mbed LLP1768
+USBMouse mouse;
